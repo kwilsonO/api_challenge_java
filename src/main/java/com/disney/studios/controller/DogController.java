85138291e61bca25e99e8a5247d4231c7ca55a27
kwilsonO <kylewilson52@gmail.com>
2016-08-03 12:33:32 -0700

    fixed the basic syntax/logic errors so it will compile

--- src/main/java/com/disney/studios/controller/DogController.java
+++ src/main/java/com/disney/studios/controller/DogController.java
@@ -5,10 +5,7 @@ import java.util.concurrent.atomic.AtomicLong;
 import com.disney.studios.Utils;
 import com.disney.studios.domain.Dog;
 import com.disney.studios.repository.DogRepository;
-import com.disney.studios.service.DogService;
-import com.sun.media.sound.ModelMappedInstrument;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,7 +14,7 @@ public class DogController {
 	@Autowired
 	private DogRepository dogRepository;
 
-	@RequestMapping(value = "/dogs", RequestMethod.GET)
+	@RequestMapping(value = "/dogs", method = RequestMethod.GET)
 	public @ResponseBody String listDog(){
 
 			//how to check for errors?
@@ -29,7 +26,7 @@ public class DogController {
 
 	}
 
-	@RequestMapping(value = "/dog/breed/{breed}", RequestMethod.GET)
+	@RequestMapping(value = "/dog/breed/{breed}", method = RequestMethod.GET)
 	public @ResponseBody String listDogsOfBreed(@PathVariable String breed){
 
 		Iterable<Dog> iter = dogRepository.findByBreed(breed);
@@ -37,15 +34,15 @@ public class DogController {
 		return Utils.jsonify((Object)iter);
 	}
 
-	@RequestMapping(value = "/dog/favorite/{url}", RequestMethod.PUT)
+	@RequestMapping(value = "/dog/favorite/{url}", method = RequestMethod.PUT)
 	public @ResponseBody String favoriteDog(@PathVariable String url){
 
 
 		String dec_url = Utils.decodeUrl(url);
 
-		int id = Integer.parseInt(dogRepository.getIdByUrl(dec_url));
+		Long id = Long.parseLong(dogRepository.getIdByUrl(dec_url));
 
-		Dog d = dogRepository.findOne(d.getId());
+		Dog d = dogRepository.findOne(id);
 
 		if(d == null){
 			return Utils.jsonifyRes("Could not find dog");
@@ -57,17 +54,17 @@ public class DogController {
 		return Utils.jsonifyRes("Successfully favorited dog");
 	}
 
-	@RequestMapping(value = "/dog/upvote/{url}", RequestMethod.PUT)
+	@RequestMapping(value = "/dog/upvote/{url}", method = RequestMethod.PUT)
 	public @ResponseBody String upvoteDog(@PathVariable String url){
 
 		String dec_url = Utils.decodeUrl(url);
 
-		int id = Integer.parseInt(dogRepository.getIdByUrl(dec_url));
+		Long id = Long.parseLong(dogRepository.getIdByUrl(dec_url));
 
-		Dog d = dogRepository.findOne(d.getId());
+		Dog d = dogRepository.findOne(id);
 
 		if(d == null) {
-			return Utils.jsonifyRes("Could not find dog").
+			return Utils.jsonifyRes("Could not find dog");
 		}
 
 		d.upvote();
@@ -77,14 +74,14 @@ public class DogController {
 		return Utils.jsonifyRes("Succesfully upvoted dog");
 	}
 
-	@RequestMapping(value = "/dog/downvote/{url}", RequestMethod.PUT)
+	@RequestMapping(value = "/dog/downvote/{url}", method = RequestMethod.PUT)
 	public @ResponseBody String downvoteDog(@PathVariable String url){
 
 		String dec_url = Utils.decodeUrl(url);
 
-		int id = Integer.parseInt(dogRepository.getIdByUrl(dec_url));
+		Long id = Long.parseLong(dogRepository.getIdByUrl(dec_url));
 
-		Dog d = dogRepository.findOne(d.getId());
+		Dog d = dogRepository.findOne(id);
 
 		if(d == null){
 			return Utils.jsonifyRes("Could not find dog");
