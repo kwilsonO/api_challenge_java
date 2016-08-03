44d30eeb0dfcd919135024279a5be14f823a4fca
kwilsonO <kylewilson52@gmail.com>
2016-08-03 12:28:13 -0700

    added code to convert responses to json as well as do some very limited error checking.

--- src/main/java/com/disney/studios/Utils.java
+++ src/main/java/com/disney/studios/Utils.java
@@ -1,22 +1,27 @@
 package com.disney.studios;
 
+import com.disney.studios.domain.Dog;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import javax.validation.OverridesAttribute;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 
 
-public class Utils{
+public class Utils {
 
 
-	public static String encodeUrl(String url){
+	public static String encodeUrl(String url) {
 
 		String encUrl = url;
 
-		try{
+		try {
 
 			encUrl = URLEncoder.encode(url, "UTF-8");
 
-		}catch(UnsupportedEncodingException e){
+		} catch (UnsupportedEncodingException e) {
 
 			System.err.println(e);
 		}
@@ -25,21 +30,61 @@ public class Utils{
 
 	}
 
-	public static String decodeUrl(String url){
+	public static String decodeUrl(String url) {
 
 		String decUrl = url;
 
-		try{
+		try {
 
 			decUrl = URLEncoder.encode(url, "UTF-8");
 
-		}catch(UnsupportedEncodingException e){
+		} catch (UnsupportedEncodingException e) {
 
 			System.err.println(e);
 		}
 
 		return decUrl;
+	}
+
+	public static String jsonify(Object o) {
+
+		ObjectMapper mapper = new ObjectMapper();
+		String json;
+
+		try {
+
+			json = mapper.writeValueAsString(o);
+
+		} catch (Exception e) {
+
+			System.err.println(e);
+			return jsonifyRes("Response could not be converted to json");
+		}
+
+		return json;
 
 
 	}
+
+	public static String jsonifyRes(String res) {
+
+		return new jsonres(res).toString();
+	}
+
 }
+	 class jsonres{
+			private String message;
+
+			jsonres(String str){
+				this.message = str;
+			}
+
+			@Override
+            public String toString(){
+			    StringBuilder builder = new StringBuilder();
+                builder.append("{ \"Response\" : \"");
+                builder.append(this.message);
+                builder.append("\" }");
+                return builder.toString();
+            }
+	}
--- src/main/java/com/disney/studios/controller/DogController.java
+++ src/main/java/com/disney/studios/controller/DogController.java
@@ -20,33 +20,26 @@ public class DogController {
 	@RequestMapping(value = "/dogs", RequestMethod.GET)
 	public @ResponseBody String listDog(){
 
-			String resJson = "";
-
+			//how to check for errors?
+			//if iter is empty, will just return empty json
+			//which seems fine
 			Iterable<Dog> iter = dogRepository.findAll();
 
-			//TODO: Jsonify
-			resJson = iter.toString();
+			return Utils.jsonify((Object)iter);
 
-			return resJson;
 	}
 
 	@RequestMapping(value = "/dog/breed/{breed}", RequestMethod.GET)
 	public @ResponseBody String listDogsOfBreed(@PathVariable String breed){
 
-		String resJson = "";
-
 		Iterable<Dog> iter = dogRepository.findByBreed(breed);
 
-		//TODO: Jsonify
-		resJson = iter.toString();
-
-		return resJson;
+		return Utils.jsonify((Object)iter);
 	}
 
 	@RequestMapping(value = "/dog/favorite/{url}", RequestMethod.PUT)
 	public @ResponseBody String favoriteDog(@PathVariable String url){
 
-		String resJson = "";
 
 		String dec_url = Utils.decodeUrl(url);
 
@@ -54,43 +47,38 @@ public class DogController {
 
 		Dog d = dogRepository.findOne(d.getId());
 
-		d.favorite();
+		if(d == null){
+			return Utils.jsonifyRes("Could not find dog");
+		}
 
+		d.favorite();
 		dogRepository.save(d);
 
-		//TODO: check for errors and return success msg
-		resJson = d.toString();
-
-		return resJson;
+		return Utils.jsonifyRes("Successfully favorited dog");
 	}
 
 	@RequestMapping(value = "/dog/upvote/{url}", RequestMethod.PUT)
 	public @ResponseBody String upvoteDog(@PathVariable String url){
 
-		String resJson = "";
-
 		String dec_url = Utils.decodeUrl(url);
 
 		int id = Integer.parseInt(dogRepository.getIdByUrl(dec_url));
 
 		Dog d = dogRepository.findOne(d.getId());
 
-		d = dogRepository.findOne(d.getId());
+		if(d == null) {
+			return Utils.jsonifyRes("Could not find dog").
+		}
 
 		d.upvote();
 
 		dogRepository.save(d);
 
-		//TODO: check for errors and return success msg
-		resJson = d.toString();
-
-		return resJson;
+		return Utils.jsonifyRes("Succesfully upvoted dog");
 	}
 
 	@RequestMapping(value = "/dog/downvote/{url}", RequestMethod.PUT)
 	public @ResponseBody String downvoteDog(@PathVariable String url){
-
-		String resJson = "";
 
 		String dec_url = Utils.decodeUrl(url);
 
@@ -98,14 +86,15 @@ public class DogController {
 
 		Dog d = dogRepository.findOne(d.getId());
 
+		if(d == null){
+			return Utils.jsonifyRes("Could not find dog");
+		}
+
 		d.downvote();
 
 		dogRepository.save(d);
 
-		//TODO: check for errors and return success msg
-		resJson = d.toString();
-
-		return resJson;
+		return Utils.jsonifyRes("Succesfully downvoted dog");
 	}
 
 
